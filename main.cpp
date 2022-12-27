8de520aa6390001fd2ab7915e5d8c99235d1f13c
Vanius <1297000341@qq.com>
2022-12-27 13:18:31 +0800

    完成了main.cpp的大概框架，以及系统的初始化，日志系统还待完成

--- main.cpp
+++ main.cpp
@@ -1,32 +1,26 @@
-#include <iostream>
-#include <set>
-using namespace std;
-set<pair<string, int>> s;
+#include "TokenScanner.h"
+
+void Init() {
+    myString sjtu("root");
+    if (acc_sys.ID_to_pos(sjtu) != -1) {
+        acc_sys.Register(myString("root"), myString("sjtu"), myString("root"), 7);
+    }
+    // todo: Init of Log
+}
+
 int main() {
-    int n;
-    cin >> n;
-    while (n--) {
-        string opt, A;
-        int B;
-        cin >> opt;
-        if (opt == "insert") {
-            cin >> A >> B;
-            s.insert(make_pair(A, B));
-        } else if (opt == "delete") {
-            cin >> A >> B;
-            if (s.count(make_pair(A, B)))
-                s.erase(make_pair(A, B));
-        } else {
-            cin >> A;
-            auto x = s.lower_bound(make_pair(A, 0));
-            if (x == s.end() || x->first != A) {
-                cout << "null" << endl;
-                continue;
-            }
-            for (x; x != s.end() && x->first == A; ++x)
-                cout << x->second << " ";
-            cout << endl;
+    string str;
+    char ch[1005];
+    bool flag = true;
+    Init();
+    while (flag) {
+        if (!getline(cin, str)) { break; }
+        if (str.size() > 1000) { cout << "Invalid\n"; }
+        else {
+            strcpy(ch, str.c_str());
+            Work(ch, flag);
         }
     }
+    // todo: log记录并输出
     return 0;
 }
